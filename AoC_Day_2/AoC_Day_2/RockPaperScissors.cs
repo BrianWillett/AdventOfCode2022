148d58db70930316fbc944702f98fdce6fe84b7a
Brian W <ugintheplaneswalker@gmail.com>
2022-12-02 09:55:46 -0400

    day 2 part 2

--- AoC_Day_2/AoC_Day_2/RockPaperScissors.cs
+++ AoC_Day_2/AoC_Day_2/RockPaperScissors.cs
@@ -8,7 +8,7 @@ namespace AoC_Day_2 {
     internal class RockPaperScissors {
 
         Dictionary<string, int> playValues = new Dictionary<string, int>();
-        Dictionary<string, string> playNames = new Dictionary<string, string>();     
+        Dictionary<string, string> playNames = new Dictionary<string, string>();
 
         int a, x = 1; //"rock"
         int b, y = 2;//"paper"
@@ -17,6 +17,8 @@ namespace AoC_Day_2 {
         int win = 6;
         int tie = 3;
         int loss = 0;
+
+        string currPlay = "";
 
         public RockPaperScissors() {
             playValues.Add("A", 1);
@@ -27,20 +29,31 @@ namespace AoC_Day_2 {
             playValues.Add("Y", 2);
             playValues.Add("Z", 3);
 
+            //part 1
+            //playNames.Add("A", "rock");
+            //playNames.Add("B", "paper");
+            //playNames.Add("C", "scissors");
+
+            //playNames.Add("X", "rock");
+            //playNames.Add("Y", "paper");
+            //playNames.Add("Z", "scissors");
+
+            //part 2
             playNames.Add("A", "rock");
             playNames.Add("B", "paper");
             playNames.Add("C", "scissors");
 
-            playNames.Add("X", "rock");
-            playNames.Add("Y", "paper");
-            playNames.Add("Z", "scissors");
+            playNames.Add("X", "loss");
+            playNames.Add("Y", "tie");
+            playNames.Add("Z", "win");
         }
 
         public int doRound(string plays) {
             int points = 0;
             var thePlays = plays.Split(" ");
             points += calculateWinner(playNames[thePlays[0]], playNames[thePlays[1]]);
-            points += playValues[thePlays[1]];
+            string myPlay = playNames.FirstOrDefault(x => x.Value == currPlay).Key; //part 2
+            points += playValues[myPlay]; //part 2
 
             return points;
         }
@@ -48,13 +61,16 @@ namespace AoC_Day_2 {
 
         private int calculateWinner(string oppPlay, string myPlay) {
 
+            myPlay = determineMyPlay(myPlay, oppPlay); //part 2
+            currPlay = myPlay;
             string outcome = "";
 
+            // part 1
             switch (myPlay) {
                 case "rock":
-                    if("paper" == oppPlay) {
+                    if ("paper" == oppPlay) {
                         outcome = "loss";
-                    }else if ("scissors" == oppPlay) {
+                    } else if ("scissors" == oppPlay) {
                         outcome = "win";
                     } else {
                         outcome = "tie";
@@ -79,6 +95,8 @@ namespace AoC_Day_2 {
                     }
                     break;
             }
+
+
             if ("" != outcome) {
                 if ("loss" == outcome) {
                     return loss;
@@ -91,6 +109,44 @@ namespace AoC_Day_2 {
                 Console.WriteLine("Error happened.  My play: " + myPlay + ", Opponents Play: " + oppPlay);
                 return tie;
             }
+        }
+
+        private string determineMyPlay(string play, string oppPlay) {
+            //part 2
+
+            string thePlay = "";
+
+            switch (play) {
+                case "loss":
+                    if ("paper" == oppPlay) {
+                        thePlay = "rock";
+                    } else if ("scissors" == oppPlay) {
+                        thePlay = "paper";
+                    } else {
+                        thePlay = "scissors";
+                    }
+                    break;
+                case "tie":
+                    if ("rock" == oppPlay) {
+                        thePlay = "rock";
+                    } else if ("paper" == oppPlay) {
+                        thePlay = "paper";
+                    } else {
+                        thePlay = "scissors";
+                    }
+                    break;
+                case "win":
+                    if ("scissors" == oppPlay) {
+                        thePlay = "rock";
+                    } else if ("rock" == oppPlay) {
+                        thePlay = "paper";
+                    } else {
+                        thePlay = "scissors";
+                    }
+                    break;
+            }
+
+            return thePlay;
         }
     }
 }
