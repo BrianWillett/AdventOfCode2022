93059485040f60a149fba09767c61630d5d849a4
Brian W <ugintheplaneswalker@gmail.com>
2022-12-04 10:06:58 -0400

    AoC Day 4

---
+++ AoC_Day_4/AoC_Day_4/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using AoC_Day_4;
+
+int partOneCount = 0;
+int partTwoCount = 0;
+Worker worker = new Worker();
+
+foreach (string line in System.IO.File.ReadAllLines(@"D:\Repos\AdventOfCode2022\AoC_Day_4\AoC_Day_4\assignments.txt")) {
+
+    partOneCount += worker.partOneParseLine(line); //part 1
+    partTwoCount += worker.partTwoParseLine(); //part 2
+
+
+
+}
+
+
+Console.WriteLine("Part 1:  " + partOneCount.ToString());
+Console.WriteLine("Part 2:  " + partTwoCount.ToString());
+
+
+Console.ReadKey();
---
+++ AoC_Day_4/AoC_Day_4/Worker.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AoC_Day_4 {
+    internal class Worker {
+
+        int[] rangeOne, rangeTwo;
+
+        public Worker() {
+        }
+
+        public int partOneParseLine(string line) {
+            int result = 0;
+
+            string[] commaSplit = line.Split(",");
+            string[] rangeOneSplit = commaSplit[0].Split("-");
+            string[] rangeTwoSplit = commaSplit[1].Split("-");
+
+            rangeOne = new int[] { Int32.Parse(rangeOneSplit[0]), Int32.Parse(rangeOneSplit[1]) };
+            rangeTwo = new int[] { Int32.Parse(rangeTwoSplit[0]), Int32.Parse(rangeTwoSplit[1]) };
+
+            int[] bothRanges = new int[] { Int32.Parse(rangeOneSplit[0]), Int32.Parse(rangeOneSplit[1]), Int32.Parse(rangeTwoSplit[0]), Int32.Parse(rangeTwoSplit[1]) };
+
+            Array.Sort(bothRanges);
+
+
+            if ((rangeTwo.Max() >= rangeOne.Max() && rangeTwo.Min() <= rangeOne.Min()) || (rangeOne.Max() >= rangeTwo.Max() && rangeOne.Min() <= rangeTwo.Min())) {
+                result = 1;
+            }
+
+            return result;
+        }
+
+        public int partTwoParseLine() {
+            int result = 0;
+
+            IEnumerable<int> range_one = Enumerable.Range(rangeOne.Min(), (rangeOne.Max() - rangeOne.Min() + 1));
+            IEnumerable<int> range_two = Enumerable.Range(rangeTwo.Min(), (rangeTwo.Max() - rangeTwo.Min() + 1));
+            List<int> rangerOne = new List<int>();
+            List<int> rangerTwo = new List<int>();
+
+            if (range_one.Intersect<int>(range_two).Any() || range_one.Intersect<int>(range_two).Any()) {
+                result = 1;
+            }
+
+
+
+            return result;
+        }
+    }
+}
